15ac4d704c650d7d47fb67e35625811a2808b079
Iryna Artemenko <136636205+iryna18artemenko@users.noreply.github.com>
2023-08-19 10:27:03 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function getImage() {
+    const random = Math.floor(Math.random() * 9) + 1;   
+    return `<img src="./images/${random}.jpg">`;
+}
+
+
+console.log( getImage() );
+console.log( getImage() );
+console.log( getImage() );
+console.log( getImage() );
